4c9bf023007eab65bd0fd8437993911b2113e3ea
Gio <kavonco@gmail.com>
2024-02-22 11:46:09 -0600

    Chihiro's SL. Pronouns and honorific's.
    Raw files inside of raw files.

---
+++ raw files/text/Events/Cinema/Event_Cmmu_109_653_C/BMD_Event_Cmmu_109_653_C_unwrapped.bmd.msg.h
@@ -0,0 +1,19 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int SEL_001_0_0                      = 1;
+const int MSG_002_0_0                      = 2;
+const int MSG_003_0_0                      = 3;
+const int MSG_003_5_0                      = 4;
+const int MSG_004_0_0                      = 5;
+const int MND_005_0_0                      = 6;
+const int MSG_006_0_0                      = 7;
+const int MSG_006_5_0                      = 8;
+const int MND_007_0_0                      = 9;
+const int MSG_008_0_0                      = 10;
+const int MSG_009_0_0                      = 11;
+const int MSG_009_5_0                      = 12;
+const int MSG_009_7_0                      = 13;
+const int MSG_010_0_0                      = 14;
+const int MSG_010_5_0                      = 15;
+const int MSG_010_7_0                      = 16;
+const int MND_011_0_0                      = 17;
