80f61849bd12b6de7913da90a2a2d03030deff90
miragedive <miragedive67@gmail.com>
2024-02-23 22:14:30 +0200

    Extr_330-470 Pronoun Changes
    330_020
    Line 16, 131, 253. 637, 694, 994: kun-> chan,
    Line 102, 472: Mr->Miss,
    
    330_030
    Line 57, 105, 200, 286: kun-> chan + He->she,
    Line 151:kun->chan + him->her,
    Line 244, 332: kun->chan + He->She + guy->girl
    
    400_010
    Line 65: kun->chan
    
    400_024
    Line 2, 35, 46: kun->chan
    
    410_020
    Line 36: kun->chan
    
    410_060
    Line 39: kun->chan
    
    410_070
    Line 2: kun->chan
    
    450_010
    Line 8: "a woman's heart" -> "how to get closer to others"
    
    460_010
    Line 2: Waiter -> Waitress
    
    460_041
    Line 11: kun->chan

---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/en/Xrd777/Events/Cinema/Event_Extr_460_010_C/Script/BMD_Scr_Event_Extr_460_010_C_unwrapped.bmd.msg.h
@@ -0,0 +1,13 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MND_003_0_0                      = 0;
+const int MND_004_0_0                      = 1;
+const int MND_004_1_0                      = 2;
+const int MND_005_0_0                      = 3;
+const int MND_006_0_0                      = 4;
+const int MND_007_0_0                      = 5;
+const int MSG_008_0_0                      = 6;
+const int MND_009_0_0                      = 7;
+const int MSG_010_0_0                      = 8;
+const int MND_011_0_0                      = 9;
+const int MSG_013_0_0                      = 10;
+const int MND_014_0_0                      = 11;
