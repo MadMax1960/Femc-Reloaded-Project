9026aeff50ed8e695cd3011e69a69851c68a29e6
miragedive <miragedive67@gmail.com>
2024-02-25 00:57:43 +0200

    FR Localisation: Main_100
    Main_100_040
    Line 22: sûr->sûre
    
    Main_100_060
    Line 9: "un nouvel"->"une nouvelle",
    Line 10: il->elle, "le dortoire des garçons"->"le doirtoire",
    Line 26: Enchanté->Enchantée, Line 28: "C'est le dortoire des filles?"->"C'est le dortoir seulement des filles?"
    
    Main_100_080
    Line 2: réveillé->réveillée
    
    Main_100_110
    Line 2: le nouvel élève -> la nouvelle élève,
    Line 18: ravi->ravie,
    Line 26: "un vrai garnement"->"une vraie friponne"
    
    Main_100_120
    Line 13: venu->venue
    
    Main_100_140
    Line 8: mec->meuf
    
    Main_100_160
    Line 2: il->elle,
    Line 5: nouvel invité -> nouvelle invitée,
    Line 71: épuisé->épuisée
    
    Main_100_170
    Line 3, 6, 9: il->elle
    
    Main_100_200
    Line 2,9,12-13, 15: il->elle,
    Line 15: ciblé->ciblée,
    Line 21: le->la
    
    Main_100_220
    Line 23: mec->meuf
    
    Main_100_225
    Line 3: habitué->habituée
    
    Main_100_230
    Line 2,5,17: il->elle,
    Line 5: épuisé-> épuisée, allé->allée,
    Line 20: le->la
    
    Main_100_240
    Line 26: le->la
    
    Main_100_285
    Line 38: maître->maîtresse

---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/fr/Xrd777/Events/Cinema/Event_Main_100_110_C/BMD_Event_Main_100_110_C_unwrapped.bmd.msg.h
@@ -0,0 +1,11 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int MSG_001_0_0                      = 1;
+const int MSG_002_0_0                      = 2;
+const int MSG_003_0_0                      = 3;
+const int SEL_004_0_0                      = 4;
+const int MSG_005_0_0                      = 5;
+const int MSG_005_3_0                      = 6;
+const int MSG_005_5_0                      = 7;
+const int MND_005_7_0                      = 8;
+const int MSG_006_0_0                      = 9;
