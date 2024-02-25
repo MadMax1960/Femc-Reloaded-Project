d8fc4c3e685f3af3d3b6ad3ebd538d363c8175c7
Gio <kavonco@gmail.com>
2024-02-24 19:00:19 -0600

    Overworld Dialogue changed- pronouns + more accurate text boxes
    Great vegetables It's a lot of files, this branch should create new folders.
    Raw files in raw files
    
    L10N>EN> Xrd777>Field>Data>DataAsset> BF>Obj>NPC

---
+++ raw files/text/Community/Bf/BMD_kfev600_CmmuNPC_016_900_cv_unwrapped.bmd.msg.h
@@ -0,0 +1,8 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int MSG_001_0_0                      = 1;
+const int MSG_002_0_0                      = 2;
+const int MSG_003_0_0                      = 3;
+const int MSG_004_0_0                      = 4;
+const int MSG_005_0_0                      = 5;
+const int MSG_006_0_0                      = 6;
