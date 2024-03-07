c32955c2ad7d4cfa724e74df89c21b38ce87b995
Laila <160956435+miragedive@users.noreply.github.com>
2024-03-07 03:40:40 +0200

    Proofreading for Events, Field, Movies, and Battle folders + Community folder fixes
    Details on spreadsheet where applicable, other details (esp for field and battle files) will be uploaded to Discord tomorrow.

---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/en/Xrd777/Battle/Event/710/BMD_BtlEvent710_unwrapped.bmd.msg.h
@@ -0,0 +1,10 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_710_100                      = 0;
+const int MSG_710_200                      = 1;
+const int MSG_710_300                      = 2;
+const int MSG_710_400                      = 3;
+const int MSG_710_500                      = 4;
+const int MSG_710_600                      = 5;
+const int MSG_710_700                      = 6;
+const int MSG_710_800                      = 7;
+const int MSG_710_900                      = 8;
---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/en/Xrd777/Battle/Event/734/BMD_BtlEvent734_unwrapped.bmd.msg.h
@@ -0,0 +1,12 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_734_0_1                      = 0;
+const int MSG_734_0_2                      = 1;
+const int MSG_734_2_0                      = 2;
+const int MND_734_0_3                      = 3;
+const int MSG_734_0_4                      = 4;
+const int TRL_734_1_1                      = 5;
+const int TRL_734_1_2                      = 6;
+const int TRL_734_1_3                      = 7;
+const int TRL_734_1_4                      = 8;
+const int TRL_734_1_5                      = 9;
+const int MND_734_0_5                      = 10;
