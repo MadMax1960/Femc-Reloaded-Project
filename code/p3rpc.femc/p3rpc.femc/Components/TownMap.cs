eea2d37cbbdee70b2657bf09c1794835411f5044
Rirurin <rirurin@rirurin.com>
2024-03-08 19:35:21 +0800

    Added battle UI blueprints

--- code/p3rpc.femc/p3rpc.femc/Components/TownMap.cs
+++ code/p3rpc.femc/p3rpc.femc/Components/TownMap.cs
@@ -5,17 +5,24 @@ using Reloaded.Hooks.Definitions.X64;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using static Reloaded.Hooks.Definitions.X64.FunctionAttribute;
 
 namespace p3rpc.femc.Components
 {
+    [StructLayout(LayoutKind.Explicit, Size = 0x428)]
+    public unsafe struct FTownMapMarker2
+    {
+        [FieldOffset(0xf0)] public FSprColor IconColor;
+    }
     public class TownMap : ModuleBase
     {
         // In AUITownMapActor::DrawTownMapUIInner
         private string AUITownMapActor_TownMapTextColor_SIG = "48 8D 54 24 ?? 89 44 24 ?? 48 8D 8F ?? ?? ?? ?? E8 ?? ?? ?? ??";
         private string AUITownMapActor_TownMapBorderColor_SIG = "48 8D 54 24 ?? 89 44 24 ?? 48 8D 8F ?? ?? ?? ?? 89 45 ??";
+        private string FTownMapMarker2_UpdateState_SIG = "48 89 5C 24 ?? 57 48 83 EC 50 48 8D B9 ?? ?? ?? ??";
 
         private UICommon _uiCommon;
 
@@ -24,6 +31,8 @@ namespace p3rpc.femc.Components
 
         private IReverseWrapper<AUITownMapActor_TownMapSetUICompColor> _townMapTextColorWrapper;
         private IReverseWrapper<AUITownMapActor_TownMapSetUICompColor> _townMapBorderColorWrapper;
+
+        private IHook<FTownMapMarker2_UpdateState> _townMapMarkerUpdateState;
 
         public unsafe TownMap(Context context, Dictionary<string, ModuleBase> modules) : base(context, modules)
         {
@@ -45,6 +54,8 @@ namespace p3rpc.femc.Components
                 };
                 _townMapBorderColor = _context._hooks.CreateAsmHook(function, addr, AsmHookBehaviour.ExecuteFirst).Activate();
             });
+            // todo: recolor marker icons for T_UI_TownMap_00_texture to a color that's visible
+            //_context._utils.SigScan(FTownMapMarker2_UpdateState_SIG, "FTownMapMarker2::UpdateState", _context._utils.GetDirectAddress, addr => _townMapMarkerUpdateState = _context._utils.MakeHooker<FTownMapMarker2_UpdateState>(FTownMapMarker2_UpdateStateImpl, addr));
         }
         public override void Register()
         {
@@ -54,7 +65,15 @@ namespace p3rpc.femc.Components
         private unsafe FSprColor AUITownMapActor_TownMapTextColorImpl() => _uiCommon.ToFSprColor(_context._config.TownMapTextColor);
         private unsafe FSprColor AUITownMapActor_TownMapBorderColorImpl() => _uiCommon.ToFSprColor(_context._config.TownMapBorderColor);
 
+        private unsafe void FTownMapMarker2_UpdateStateImpl(FTownMapMarker2* self, float deltaTime)
+        {
+            _townMapMarkerUpdateState.OriginalFunction(self, deltaTime);
+            _uiCommon.SetColor(ref self->IconColor, _context.ColorWhite);
+        }
+
         [Function(new Register[] {}, FunctionAttribute.Register.rax, false)]
         private unsafe delegate FSprColor AUITownMapActor_TownMapSetUICompColor();
+
+        private unsafe delegate void FTownMapMarker2_UpdateState(FTownMapMarker2* self, float deltaTime);
     }
 }
