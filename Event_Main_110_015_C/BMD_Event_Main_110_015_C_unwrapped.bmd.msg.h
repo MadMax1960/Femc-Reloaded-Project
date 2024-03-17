c50ad7ca85afd724cea78c52e2761315f20746e1
Yoshi2000 <160791833+TheYoshi2000@users.noreply.github.com>
2024-03-18 00:36:02 +0100

    Add files via upload

---
+++ Event_Main_110_015_C/BMD_Event_Main_110_015_C_unwrapped.bmd.msg.h
@@ -0,0 +1,7 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int SEL_001_0_0                      = 1;
+const int MSG_002_0_0                      = 2;
+const int MSG_003_0_0                      = 3;
+const int MSG_004_0_0                      = 4;
+const int MSG_005_0_0                      = 5;
