291737dae197a73b9ec87f29bdb57de5b9c91322
kiawaii <163614719+kiawaii@users.noreply.github.com>
2024-03-19 19:52:02 -0300

    Add files via upload

---
+++ BMD_Event_Cmmu_010_737_C_unwrapped.bmd.msg.h
@@ -0,0 +1,12 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_011_0_0                      = 0;
+const int MSG_012_0_0                      = 1;
+const int SEL_013_0_0                      = 2;
+const int MSG_014_0_0                      = 3;
+const int MSG_015_0_0                      = 4;
+const int MSG_016_0_0                      = 5;
+const int MSG_017_0_0                      = 6;
+const int MSG_018_0_0                      = 7;
+const int MSG_019_0_0                      = 8;
+const int MSG_019_5_0                      = 9;
+const int MSG_020_0_0                      = 10;
