1bda888313e30d24382dcfa7e70b166494dd5d1c
Yoshi2000 <160791833+TheYoshi2000@users.noreply.github.com>
2024-03-31 01:36:58 +0100

    Yuko Social Link - ES
    All of the files needed for Yuko's S. Link for the spanish translation

---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/es/Xrd777/Events/Cinema/Event_Cmmu_106_520_C/BMD_Event_Cmmu_106_520_C_unwrapped.bmd.msg.h
@@ -0,0 +1,6 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MSG_000_0_0                      = 0;
+const int MSG_000_3_0                      = 1;
+const int MSG_000_5_0                      = 2;
+const int MSG_000_7_0                      = 3;
+const int MND_001_0_0                      = 4;
