6f7e11ab806294983c9533b16a1c82f6bb504526
Yoshi2000 <160791833+TheYoshi2000@users.noreply.github.com>
2024-04-13 01:26:55 +0200

    Field_Dungeon_Hit-ES
    All of the files needed for the Hit folder inside the Field folder of the spanish translation

---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/es/Xrd777/Field/Data/DataAsset/Bf/Dungeon/Hit/BMD_FldHit_OperationsRoom_11_22_unwrapped.bmd.msg.h
@@ -0,0 +1,21 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int SEL_BOSS                         = 0;
+const int MSG_OPE_STOP_YUKARI              = 1;
+const int MSG_OPE_STOP_AEGIS               = 2;
+const int MSG_OPE_STOP_MITSURU             = 3;
+const int MSG_OPE_STOP_SANADA              = 4;
+const int MSG_OPE_STOP_AMADA               = 5;
+const int MSG_OPE_STOP_FUKA                = 6;
+const int MSG_GOTO_OPE                     = 7;
+const int MSG_FUKA_LETS_GO_02              = 8;
+const int MND_BOOK_SHELF_01                = 9;
+const int MND_BOOK_SHELF_02                = 10;
+const int MND_BOOK_SHELF_03                = 11;
+const int MND_NOZOKI_ANYONE1               = 12;
+const int MND_NOZOKI_ANYONE2               = 13;
+const int SEL_YESNO                        = 14;
+const int MSG_OPE_STOP_JUNPEI              = 15;
+const int MSG_FUKA_LETS_GO_01              = 16;
+const int MSG_1103_MITURU_01               = 17;
+const int MSG_1103_IKUTUKI_01              = 18;
+const int MSG_1103_MITURU_02               = 19;
