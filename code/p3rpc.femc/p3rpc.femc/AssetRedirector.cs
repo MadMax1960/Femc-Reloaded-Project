8c0a3c56d62257ffad1d9d1cc3ef59f462e821cf
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2024-08-31 01:19:38 -0400

    all asset redirectior stuff in asset redirector.cs

--- code/p3rpc.femc/p3rpc.femc/AssetRedirector.cs
+++ code/p3rpc.femc/p3rpc.femc/AssetRedirector.cs
@@ -21,13 +21,13 @@ namespace p3rpc.femc
 		{
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_F000", "/Game/Xrd777/Characters/Player/PC0002/Models/SK_PC0002_F999"); // face
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_H000", "/Game/Xrd777/Characters/Player/PC0002/Models/SK_PC0002_H999"); // hair
-			// this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_H000", "/Game/Xrd777/Characters/Player/PC0002/Models/SK_PC0002_H998"); // aigis hair
+		    //this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_H000", "/Game/Xrd777/Characters/Player/PC0002/Models/SK_PC0002_H998"); // aigis hair
 			this.RedirectAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_CombineAnim", "/Game/Xrd777/Characters/Data/DataAsset/Player/PC0002/DA_PC0002_CombineAnim"); // idk what this is, its something
 			this.RedirectAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_CommonAnim", "/Game/Xrd777/Characters/Data/DataAsset/Player/PC0002/DA_PC0002_CommonAnim"); // common anim stuff, walking, sitting, griddying, etc
 			//this.RedirectAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_EventAnim", "/Game/Xrd777/Characters/Data/DataAsset/Player/PC0002/DA_PC0002_EventAnim"); // Event anims, so specific events, probably why velvet room dies tbqh
 			this.RedirectAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_FaceAnim", "/Game/Xrd777/Characters/Data/DataAsset/Player/PC0002/DA_PC0002_FaceAnim"); // read the filename to my left
 			//this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/ABP_PC0001", "/Game/Xrd777/Characters/Player/PC0002/ABP_PC0002"); // might omega die
-			// this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/AnmCommon/A_PC0001_CMA0055_ADDP_BagLNoPocket", "/Game/Xrd777/Characters/Player/PC0002/AnmCommon/A_PC0002_CMA0056_ADDP_BagL"); // might die 
+			//this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/AnmCommon/A_PC0001_CMA0055_ADDP_BagLNoPocket", "/Game/Xrd777/Characters/Player/PC0002/AnmCommon/A_PC0002_CMA0056_ADDP_BagL"); // might die 
 			//this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/AnmCommon/A_PC0001_CMA0056_ADDP_BagL", "/Game/Xrd777/Characters/Player/PC0002/AnmCommon/A_PC0002_CMA0056_ADDP_BagL"); // might die 2
 			//this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/AnmCommon/A_PC0001_CMA0063_ADDP_TravelBagL", "/Game/Xrd777/Characters/Player/PC0002/AnmCommon/A_PC0002_CMA0056_ADDP_BagL"); // might die 3
 			//this.RedirectAsset("/Game/Xrd777/Blueprints/Characters/Player/Bag/BP_AppCharBag_0001_000", "/Game/Xrd777/Blueprints/Characters/Player/Bag/BP_AppCharBag_0002_000"); // idk what these 2 do
@@ -70,4 +70,158 @@ namespace p3rpc.femc
 			_unreal.AssignFName(_modName, ogFnames.AssetPath, newFnames.AssetPath);
 		}
 	}
+
+	internal static class AssetUtils
+	{
+		public static string GetAssetPath(Character character, AssetType type, int costumeId = 0)
+			=> type switch
+			{
+				AssetType.BaseMesh => FormatAssetPath($"/Game/Xrd777/Characters/Player/PC{FormatCharId(character)}/Models/SK_PC{FormatCharId(character)}_BaseSkelton", character),
+				AssetType.CostumeMesh => FormatAssetPath($"/Game/Xrd777/Characters/Player/PC{FormatCharId(character)}/Models/SK_PC{FormatCharId(character)}_C{costumeId:000}", character),
+				AssetType.HairMesh => FormatAssetPath($"/Game/Xrd777/Characters/Player/PC{FormatCharId(character)}/Models/SK_PC{FormatCharId(character)}_H{costumeId:000}", character),
+				AssetType.FaceMesh => FormatAssetPath($"/Game/Xrd777/Characters/Player/PC{FormatCharId(character)}/Models/SK_PC{FormatCharId(character)}_F{costumeId:000}", character),
+				AssetType.CommonAnim => FormatAssetPath($"/Game/Xrd777/Characters/Data/DataAsset/Player/PC{FormatCharId(character)}/DA_PC{FormatCharId(character)}_CommonAnim", character),
+				AssetType.CombineAnim => FormatAssetPath($"/Game/Xrd777/Characters/Data/DataAsset/Player/PC{FormatCharId(character)}/DA_PC{FormatCharId(character)}_CombineAnim", character),
+				AssetType.EventAnim => FormatAssetPath($"/Game/Xrd777/Characters/Data/DataAsset/Player/PC{FormatCharId(character)}/DA_PC{FormatCharId(character)}_EventAnim", character),
+				AssetType.FaceAnim => FormatAssetPath($"/Game/Xrd777/Characters/Data/DataAsset/Player/PC{FormatCharId(character)}/DA_PC{FormatCharId(character)}_FaceAnim", character),
+				_ => throw new Exception(),
+			};
+
+		public static string GetAssetFile(Character character, AssetType type, Outfit outfit)
+			=> GetAssetPath(character, type, (int)outfit);
+
+		public static string GetAssetPath(string assetFile)
+		{
+			var adjustedPath = assetFile.Replace('\\', '/').Replace(".uasset", string.Empty);
+
+			if (adjustedPath.IndexOf("Content") is int contentIndex && contentIndex > -1)
+			{
+				adjustedPath = adjustedPath.Substring(contentIndex + 8);
+			}
+
+			if (!adjustedPath.StartsWith("/Game/"))
+			{
+				adjustedPath = $"/Game/{adjustedPath}";
+			}
+
+			return adjustedPath;
+		}
+
+		public static string FormatCharId(Character character)
+			=> ((int)character).ToString("0000");
+
+		private static string FormatAssetPath(string path, Character character)
+		{
+			if (character >= Character.Player && character <= Character.Shinjiro)
+			{
+				return path;
+			}
+			else
+			{
+				return path.Replace("PC", "SC").Replace("Player", "Sub");
+			}
+		}
+	}
+
+	public enum AssetType
+	{
+		BaseMesh,
+		CostumeMesh,
+		HairMesh,
+		FaceMesh,
+
+		CommonAnim,
+		CombineAnim,
+		EventAnim,
+		FaceAnim,
+	}
+
+	public enum Character
+	{
+		NONE,
+		Player,
+		Yukari,
+		Stupei,
+		Akihiko,
+		Mitsuru,
+		Fuuka,
+		Aigis,
+		Ken,
+		Koromaru,
+		Shinjiro = 10,
+		FEMC = 999,
+	}
+
+	public class CharacterConfig
+	{
+		public CharacterBase Base { get; set; } = new();
+
+		public CharacterAnims Animations { get; set; } = new();
+
+		public Dictionary<Outfit, string?> Outfits { get; set; } = new();
+	}
+
+	public class CharacterBase
+	{
+		public Character Character { get; set; } = Character.Player;
+
+		public string? BaseSkeleton { get; set; }
+
+		public string? Hair { get; set; }
+
+		public string? Face { get; set; }
+	}
+
+	public class CharacterAnims
+	{
+		public string? Common { get; set; }
+
+		public string? Combine { get; set; }
+
+		public string? Event { get; set; }
+
+		public string? Face { get; set; }
+	}
+
+	public enum Outfit
+	{
+		Missing = 0,
+		Summer_Uniform = 1,
+		Winter_Uniform = 2,
+		Summer_Casual = 5,
+		Winter_Casual = 6,
+		Uniform_Armband = 51,
+		SEES_Uniform = 52,
+		Gekkoukan_Jersey = 101,
+		Swimsuit = 102,
+		Nightwear = 103,
+		Battle_Armor = 104,
+		Butler_Suit = 106,
+		Track_Team_Shirt = 151,
+		Hot_Springs_Towel = 154,
+		Hotel_Yukata = 155,
+		Shrine_Festival_Yukata = 156,
+		New_Years_Kimono = 157,
+		Wilduck_Burger_Uniform = 158,
+		Dorm_Apron = 159,
+		Cafe_Uniform = 160,
+		Be_Blue_V_Uniform = 161,
+		Screen_Shot_Uniform = 162,
+		SEES_Outfit = 201,
+		SEES_Uniform_2 = 202,
+		Damaged_Ribbon = 203,
+		Ocean_Sundress = 204,
+		Ocean_Sundress_2 = 205,
+		Firearms = 206,
+		Phantom_Suit = 501,
+		Shujin_Uniform = 502,
+		Yasogami_Uniform = 503,
+	}
+
+	internal record AssetFNames(string AssetFile)
+	{
+		public string AssetName { get; } = Path.GetFileNameWithoutExtension(AssetFile);
+
+		public string AssetPath { get; } = AssetUtils.GetAssetPath(AssetFile);
+	};
 }
