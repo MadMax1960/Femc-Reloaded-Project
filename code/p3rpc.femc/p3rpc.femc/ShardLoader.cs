5ed3ee3920dc1ba4ef99ae258743ff117f6cc426
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-02-09 01:07:26 -0500

    shard in it's own cs

---
+++ code/p3rpc.femc/p3rpc.femc/ShardLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using p3rpc.femc.Configuration;
+using UnrealEssentials.Interfaces;
+using static p3rpc.femc.Configuration.Config;
+
+namespace p3rpc.femc
+{
+	public static class ShardLoader
+	{
+		public static void LoadShardAssets(IUnrealEssentials unrealEssentials, Config configuration, string modLocation)
+		{
+			if (configuration.ShardTrue == ShardType.Esa)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "Shard", "Esa"));
+			else if (configuration.ShardTrue == ShardType.Ely)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "Shard", "Ely"));
+			else if (configuration.ShardTrue == ShardType.ElyAlt)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "Shard", "ElyAlt"));
+			else if (configuration.ShardTrue == ShardType.Shiosakana)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "Shard", "Shiosakana"));
+			else if (configuration.ShardTrue == ShardType.namiweiko)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "Shard", "namiweiko"));
+			// Add other Shard conditions if needed.
+		}
+	}
+}
