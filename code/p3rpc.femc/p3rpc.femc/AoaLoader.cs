df798df702d2b3315c007e7c754ab2f71085a26a
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-02-09 01:03:06 -0500

    Aoa and aoa text in their own cs

---
+++ code/p3rpc.femc/p3rpc.femc/AoaLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using p3rpc.femc.Configuration;
+using UnrealEssentials.Interfaces;
+using static p3rpc.femc.Configuration.Config;
+
+namespace p3rpc.femc
+{
+	public static class AoaLoader
+	{
+		public static void LoadAoaAssets(IUnrealEssentials unrealEssentials, Config configuration, string modLocation)
+		{
+			if (configuration.AOATrue == AOAType.Ely)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "Ely"));
+			else if (configuration.AOATrue == AOAType.Chrysanthie)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "Chrysanthie"));
+			else if (configuration.AOATrue == AOAType.Fernando)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "Fernando"));
+			else if (configuration.AOATrue == AOAType.Monica)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "Monica"));
+			else if (configuration.AOATrue == AOAType.RonaldReagan)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "RonaldReagan"));
+			else if (configuration.AOATrue == AOAType.esaadrien)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "esaadrien"));
+			else if (configuration.AOATrue == AOAType.mekki)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "mekki"));
+			else if (configuration.AOATrue == AOAType.shiosakana)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "shiosakana"));
+			else if (configuration.AOATrue == AOAType.shiosakanaAlt)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "shiosakanaAlt"));
+			else if (configuration.AOATrue == AOAType.Nami)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOA", "Nami"));
+			// Add other AOA conditions if needed.
+		}
+	}
+}
---
+++ code/p3rpc.femc/p3rpc.femc/AoaTextLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using p3rpc.femc.Configuration;
+using UnrealEssentials.Interfaces;
+using static p3rpc.femc.Configuration.Config;
+
+namespace p3rpc.femc
+{
+	public static class AoaTextLoader
+	{
+		public static void LoadAoaTextAssets(IUnrealEssentials unrealEssentials, Config configuration, string modLocation)
+		{
+			if (configuration.AOAText == AOATextType.DontLookBack)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOAText", "DontLookBack"));
+			else if (configuration.AOAText == AOATextType.SorryBoutThat)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOAText", "SorryBoutThat"));
+			else if (configuration.AOAText == AOATextType.PerfectlyAccomplished)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "AOAText", "PerfectlyAccomplished"));
+			// Add other AOAText conditions if needed.
+		}
+	}
+}
