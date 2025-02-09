d04d9a157a9bda19d7abbbbd74f2866888c36975
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-02-09 01:11:51 -0500

    Party Panel in it's own cs

---
+++ code/p3rpc.femc/p3rpc.femc/PartyPanelLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using p3rpc.femc.Configuration;
+using UnrealEssentials.Interfaces;
+using static p3rpc.femc.Configuration.Config;
+
+namespace p3rpc.femc
+{
+	public static class PartyPanelLoader
+	{
+		public static void LoadPartyPanelAssets(IUnrealEssentials unrealEssentials, Config configuration, string modLocation)
+		{
+			if (configuration.PartyPanelTrue == PartyPanelType.Kris)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "PartyPanel", "Kris"));
+			else if (configuration.PartyPanelTrue == PartyPanelType.Esa)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "PartyPanel", "Esa"));
+			// Add other PartyPanel conditions if needed.
+		}
+	}
+}
