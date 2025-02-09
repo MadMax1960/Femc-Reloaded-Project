ef7b2e73c8b0219813eaf58b714c0c93f144c358
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-02-09 01:08:42 -0500

    Level Up Portrait in it's own cs

---
+++ code/p3rpc.femc/p3rpc.femc/LevelUpLoader.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using p3rpc.femc.Configuration;
+using UnrealEssentials.Interfaces;
+using static p3rpc.femc.Configuration.Config;
+
+namespace p3rpc.femc
+{
+	public static class LevelUpLoader
+	{
+		public static void LoadLevelUpAssets(IUnrealEssentials unrealEssentials, Config configuration, string modLocation)
+		{
+			if (configuration.LevelUpTrue == LevelUpType.Esa)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "LevelUp", "Esa"));
+			else if (configuration.LevelUpTrue == LevelUpType.Ely)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "LevelUp", "Ely"));
+			else if (configuration.LevelUpTrue == LevelUpType.shiosakana)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "LevelUp", "shiosakana"));
+			else if (configuration.LevelUpTrue == LevelUpType.ElyAlt)
+				unrealEssentials.AddFromFolder(Path.Combine(modLocation, "2d", "LevelUp", "ElyAlt"));
+			// Add other LevelUp conditions if needed.
+		}
+	}
+}
