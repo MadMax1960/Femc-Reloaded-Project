0e98323ce42b7ca603d232e7a954727c38e488a1
kiawaii <163614719+kiawaii@users.noreply.github.com>
2025-02-11 00:41:12 -0300

    More files

---
+++ p3rpc.femc/UnrealEssentials/P3R/Content/L10N/es/Xrd777/Field/Data/DataAsset/Bf/Init/KeyFreeEvent/FldKeyFreeEventAnnounce/BMD_FldKeyFreeEventAnnounce_unwrapped.bmd.msg.h
@@ -0,0 +1,15 @@
+// Decompiled by Atlus Script Tools (2017-2021) © TGE
+const int MND_000_0_0                      = 0;
+const int MND_001_0_0                      = 1;
+const int MSG_002_0_0                      = 2;
+const int MND_003_0_0                      = 3;
+const int MND_004_0_0                      = 4;
+const int MND_005_0_0                      = 5;
+const int MSG_018_0_0                      = 6;
+const int MSG_019_0_0                      = 7;
+const int MND_006_0_0                      = 8;
+const int MND_022_0_0                      = 9;
+const int MND_025_0_0                      = 10;
+const int MSG_027_0_0                      = 11;
+const int MND_029_0_0                      = 12;
+const int MSG_031_0_0                      = 13;
