0da1e258a03afe76ffee4cf1acb1ce842535c209
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-05-09 21:50:07 -0400

    returne of trent crimm

--- code/p3rpc.femc/p3rpc.femc/AssetRedirector.cs
+++ code/p3rpc.femc/p3rpc.femc/AssetRedirector.cs
@@ -22,6 +22,7 @@ namespace p3rpc.femc
 			this.RedirectAsset("/Game/Xrd777/Characters/Data/DataAsset/Player/PC0001/DA_PC0001_FaceAnim", "/Game/Xrd777/Characters/Data/DataAsset/Player/PC0002/DA_PC0002_FaceAnim"); // read the filename to my left
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_C051", "/Game/Xrd777/Characters/Player/FemC/Femc_Winter_School_Battle.uasset"); // joker persona 5 reference
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_C502", "/Game/Xrd777/Characters/Player/PC0005/Models/SK_PC0005_C502"); // mitsuru shujin redirect
+			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_BaseSkelton", "/Game/Xrd777/Characters/Player/FemC/Femc_Skeleton.uasset"); // trent crimm the indepedent
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_C504", "/Game/Xrd777/Characters/Player/PC0006/Models/SK_PC0006_C504");
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_H504", "/Game/Xrd777/Characters/Player/FemC/Femc_Hair"); // forgor velvet hair
 			this.RedirectAsset("/Game/Xrd777/Characters/Player/PC0001/Models/SK_PC0001_Title_00", "/Game/Xrd777/Characters/Player/FemC/malcomlni_n_thine_midel"); // title model
