9ef45b5218008c6f8588223a39cfd477e2d1aa04
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-07-08 22:30:58 -0400

    saori

---
+++ code/p3rpc.femc/p3rpc.femc/Saori/Saori.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using p3rpc.femc.Configuration;
+using Reloaded.Mod.Interfaces;
+using Ryo.Interfaces;
+using UnrealEssentials.Interfaces;
+
+namespace p3rpc.femc;
+
+public static class Saori
+{
+    public static void LoadSaoriAssets(
+        IUnrealEssentials unrealEssentials,
+        IModLoader modLoader,
+        IModConfig modConfig,
+        IRyoApi ryo,
+        Config configuration,
+        string modLocation)
+    {
+        {
+        }
+    }
+}
