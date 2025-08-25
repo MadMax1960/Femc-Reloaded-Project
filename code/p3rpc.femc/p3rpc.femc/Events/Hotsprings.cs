c66eb34d12773e79222466ec2cb9945d77c20322
TheBestAstroNOT <139786546+TheBestAstroNOT@users.noreply.github.com>
2025-08-25 12:31:38 +0530

    Decompile remaining files and add support for Dorm Hangouts

--- code/p3rpc.femc/p3rpc.femc/Events/Hotsprings.cs
+++ code/p3rpc.femc/p3rpc.femc/Events/Hotsprings.cs
@@ -22,6 +22,7 @@ public static class HotspringsLoader
         {
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "Hotsprings"));
             atlusAssets.RegisterAssetsFolderWithModData(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "Hotsprings", "MessageScripts"), new (modConfig.ModId, modLocation), AssetMode.Default, ESystemLanguage.UNIVERSAL);
+            atlusAssets.RegisterAssetsFolderWithModData(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "DormHang", "MessageScripts"), new(modConfig.ModId, modLocation), AssetMode.Default, ESystemLanguage.UNIVERSAL);
             //ryo.AddAudioFolder(modLoader.GetDirectoryForModId(modConfig.ModId) + "/Events/Ray/Oscar Fortnite/Hotsprings Voice");
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "DormHang"));
         }
