43b160c2a6d33363e64e79dca72fb956621f67d4
MadMax1960 <30520119+MadMax1960@users.noreply.github.com>
2025-09-04 19:15:02 -0400

    lmao

--- code/p3rpc.femc/p3rpc.femc/3D/AnimLoader.cs.cs
+++ code/p3rpc.femc/p3rpc.femc/3D/AnimLoader.cs.cs
@@ -12,7 +12,7 @@ namespace p3rpc.femc
             if (configuration.AnimTrue == AnimType.OriginalAnims)
                 unrealEssentials.AddFromFolder(Path.Combine(modLocation, "3d", "Anims", "Original Dummy")); // empty folder that just loads vanilla anims 
             else if (configuration.AnimTrue == AnimType.CustomAnims)
-                unrealEssentials.AddFromFolder(Path.Combine(modLocation, "3d", "Anims", "Custom Anims")); // aoa might be seperate from all of these maybe, depends on if we do custom ending poses per aoa 2d option. Would need to pass the 2d aoa conditionals if we do
+                //unrealEssentials.AddFromFolder(Path.Combine(modLocation, "3d", "Anims", "Custom Anims")); // aoa might be seperate from all of these maybe, depends on if we do custom ending poses per aoa 2d option. Would need to pass the 2d aoa conditionals if we do
             else if (configuration.AnimTrue == AnimType.VeryFunnyAnims)
                 unrealEssentials.AddFromFolder(Path.Combine(modLocation, "3d", "Anims", "Very Funny Anims")); // the funny anims 
         }
