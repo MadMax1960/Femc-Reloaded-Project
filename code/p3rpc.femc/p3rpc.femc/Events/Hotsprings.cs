1c40ea0d01e2af425ad5fa0cda4f0add217f472e
raycopper <kingdom.hearts@live.dk>
2026-09-23 18:43:48 +0200

    Remove unused folder code

--- code/p3rpc.femc/p3rpc.femc/Events/Hotsprings.cs
+++ code/p3rpc.femc/p3rpc.femc/Events/Hotsprings.cs
@@ -22,10 +22,7 @@ public static class HotspringsLoader
         if (configuration.TesticlesEventsDorm)
         {
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "Hotsprings"));
-            unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "DormHang"));
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "Festival"));
-            unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "LizDates"));
-            unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "AkinariSLComplete"));
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "Storytime"));
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "BeachEpisode"));
             unrealEssentials.AddFromFolder(Path.Combine(modLocation, "Events", "Ray", "Oscar Fortnite", "FuukaFMO"));
